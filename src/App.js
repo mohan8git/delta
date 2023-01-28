1c95363138cae2d255f373d0e0a94f91d2d5f681
mohan8git <bishnoimohan502@gmail.com>
2023-01-28 23:58:34 +0530

    delta-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Table } from 'antd';
+import axios from 'axios';
+import 'antd/dist/reset.css';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [symbolArray, setSymbolArray] = useState([]);
+  useEffect(() => {
+    axios.get('https://api.delta.exchange/v2/products').then((res) => {
+      setProducts(res.data.result);
+      const symbolArray = res.data.result.map((item) => {
+        return item.symbol;
+      });
+      setSymbolArray(symbolArray);
+    });
+  }, []);
+
+  const socket = new WebSocket('wss://production-esocket.delta.exchange');
+
+  socket.onopen = () => {
+    const subscribeMessage = {
+      type: 'subscribe',
+      payload: {
+        channels: [
+          {
+            name: 'v2/ticker',
+            symbols: [...symbolArray],
+          },
+        ],
+      },
+    };
+    socket.send(JSON.stringify(subscribeMessage));
+  };
+
+  setInterval(() => {
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.type === 'v2/ticker') {
+        const newProducts = products.map((item) => {
+          if (item.symbol === data.symbol) {
+            item.markPrice = data.mark_price;
+          }
+          return item;
+        });
+        setProducts(newProducts);
+      }
+    };
+  }, 1000);
+
+  const columns = [
+    {
+      title: 'Symbol',
+      dataIndex: 'symbol',
+      key: 'symbol',
+      width: '25%',
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description',
+      width: '25%',
+    },
+    {
+      title: 'Underlying Asset',
+      dataIndex: ['underlying_asset', 'symbol'],
+      key: ['underlying_asset', 'symbol'],
+      width: '15%',
+    },
+    {
+      title: 'Mark Price',
+      dataIndex: 'markPrice',
+      key: 'markPrice',
+      render: (text, record) => {
+        const currentPrice = record.markPrice;
+        return <div>{currentPrice ? currentPrice : 'loading...'}</div>;
+      },
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <div className='header'>
+        <img
+          src='https://www.delta.exchange/small-logo.svg'
+          alt='logo'
+          className='logo'
+        />
+        <div className='delta-button'>Delta Assigment</div>
+      </div>
+      <Table
+        dataSource={products}
+        columns={columns}
+        pagination={{
+          pageSize: 10,
+        }}
+        tableProps={{
+          scroll: { x: 100 + 96, y: 700 },
+        }}
+      />
+      ;
     </div>
   );
 }
